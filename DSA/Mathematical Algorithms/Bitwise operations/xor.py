fdcfb3d93ac291b6bd7572704f6cc0dd9b4ed938
deepak <deepak21082005@gmail.com>
2024-02-24 11:26:13 +0530

    added files to bitwise

--- DSA/Mathematical Algorithms/Bitwise operations/xor.py
+++ DSA/Mathematical Algorithms/Bitwise operations/xor.py
@@ -1,9 +1,28 @@
 #Find XOR of two numbers without using xor operator
 
-def xor(x,y):
+#method 1
+'''def xor(x,y):
     return ((x|y)&(~x|~y))
 
 x=int(input("Enter a number :"))
 y=int(input("Enter a number :"))
 
 print("XOR is ",xor(x,y))
+
+#method2
+
+def xor(x,y):
+    return (x+y-(2*(x&y)))
+
+x=3
+y=5
+print("XOR :",xor(x,y))'''
+
+#method 3
+
+def xor(x,y):
+    return ((x|y)-(x&y))
+
+x=int(input("Enter a number :"))
+y=int(input("Enter another number :"))
+print(x,"^",y,"is",xor(x,y))
